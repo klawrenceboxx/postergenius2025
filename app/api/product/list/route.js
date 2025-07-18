9faac3285d4a50da414622e28c155c77ec94f3ef
Kaleel Lawrence-boxx <kaleellawrence-boxx@hotmail.ca>
2025-07-18 08:31:20 -0400

    Summary: Display product list on homepage using API data - Implemented new /api/product/list route to fetch all products without seller check, Updated context to fetch and display product data via API instead of dummy assets

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,20 +1,9 @@
 import connectDB from "@/config/db";
-import authSeller from "@/lib/authSeller";
 import Product from "@/models/Product";
-import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-    // Authenticate the seller
-    const { userId } = getAuth(request);
-
-    const isSeller = authSeller(userId);
-
-    if (!isSeller) {
-      return NextResponse.json({ success: false, message: "Unauthorized" });
-    }
-
     await connectDB();
 
     const products = await Product.find({});
