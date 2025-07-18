069c81d84fafdab8a1eb843007db1864250782ea
Kaleel Lawrence-boxx <kaleellawrence-boxx@hotmail.ca>
2025-07-18 15:46:17 -0400

    missing 's' on event in inngest, changed the way i resolve the math for the cost amount

--- config/inngest.js
+++ config/inngest.js
@@ -61,7 +61,7 @@ export const createUserOrder = inngest.createFunction(
     },
   },
   { event: "order/created" },
-  async ({ event }) => {
+  async ({ events }) => {
     const orders = events.map((event) => {
       return {
         userId: event.data.userId,
