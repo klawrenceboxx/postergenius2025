a253d73af018899062bc23e00d0537ba65ed3d0d
Kaleel Lawrence-boxx <kaleellawrence-boxx@hotmail.ca>
2025-07-18 13:31:47 -0400

    Summary: implemented order creation flow: Created Order model schema, Built POST API to handle order placement, Integrated Inngest for batched order processing with max size and timeout config, Connected order creation event to Inngest handler and inserted orders into DB, Added 13% tax to order total before saving, Cleared user's cart after successful order placement

---
+++ app/api/order/create/route.js
@@ -0,0 +1,45 @@
+import Product from "@/models/Product";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
+import { inngest } from "@/config/inngest";
+
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const { address, items } = await request.json();
+
+    if (!address || !items.length === 0) {
+      return NextResponse.json({ success: false, message: "Invalid data" });
+    }
+
+    // calculate amount using items
+    const amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return acc + product.offerPrice * item.quantity;
+    }, 0);
+
+    await inngest.send({
+      name: "Order Created",
+      data: {
+        userId,
+        address,
+        items,
+        amount: amount + Math.foor(amount * 0.13), // adding 13% tax
+        date: Date.now(),
+      },
+    });
+
+    // clear user cart
+    const user = await User.findById(userId);
+    user.cartItems = [];
+    await user.save();
+
+    return NextResponse.json({
+      success: true,
+      message: "Order Created Successfully",
+    });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
---
+++ models/Order.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: String, required: true, ref: "user" },
+  items: [
+    {
+      product: { type: stringify, required: true, ref: "product" },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  amount: { type: Number, required: true },
+  address: { type: String, ref: "address", required: true },
+  status: { type: String, required: true, default: "Order Place" },
+  date: { type: Number, required: true },
+});
+
+const Order = mongoose.models.order || mongoose.model("order", orderSchema);
+
+export default Order;
